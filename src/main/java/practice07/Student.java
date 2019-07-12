f881e77cc4bce64761d5a5e85acc7cbeb1426612
ALVARLEUNG <alvar.leung@qq.com>
2019-07-12 11:03:07 +0800

    practice：finished functions of Practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,19 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(String name, int age, Klass klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return  "My name is "+this.getName()+". I am "+this.getAge()+" years old. I am a Student. I am at "+this.klass.getDisplayName()+".";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,32 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return this.klass!=null? "My name is "+this.getName()+". I am "+this.getAge()+" years old. I am a Teacher. I teach "+this.klass.getDisplayName()+"." :"My name is "+this.getName()+". I am "+this.getAge()+" years old. I am a Teacher. I teach No Class." ;
+    }
+
+    public String introduceWith(Student student) {
+
+        if (student.getKlass().getNumber() == this.klass.getNumber()) {
+            return "My name is "+this.getName()+". I am "+this.getAge()+" years old. I am a Teacher. I teach "+student.getName()+".";
+        } else {
+            return "My name is "+this.getName()+". I am "+this.getAge()+" years old. I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
 }
