4ad1c56b4d2c9782ad0aac5b7faf0e7d10d891bf
ALVARLEUNG <alvar.leung@qq.com>
2019-07-12 11:02:39 +0800

    practice：finished functions of Practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,23 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return this.klass != 0? "My name is "+this.getName()+". I am "+this.getAge()+" years old. I am a Teacher. I teach Class "+this.klass +"." :"My name is "+this.getName()+". I am "+this.getAge()+" years old. I am a Teacher. I teach No Class." ;
+    }
 }
